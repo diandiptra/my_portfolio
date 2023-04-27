aec50bf4df32e6bbfdcddcb7b1e2fc37be8db7a8
diandiptra <hardian.diptra@gmail.com>
2023-04-27 08:42:38 +0700

    Change Style Switcher JS

--- js/daynight-switch.js
+++ js/daynight-switch.js
@@ -6,12 +6,3 @@ dayNight.addEventListener('click', () => {
 
   document.body.classList.toggle("dark");
 });
-
-window.addEventListener("load", () => {
-  if (document.body.classList.contains("dark")) {
-    dayNight.querySelector("i").classList.add('fa-sun');
-  }
-  else {
-    dayNight.querySelector("i").classList.add('fa-moon');
-  }
-});
